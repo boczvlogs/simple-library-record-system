f8d844656fa46d6e4624df9d41145ce1199bc4b9
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2024-05-06 15:13:16 +0800

    Update library_system.py

--- library_system.py
+++ library_system.py
@@ -53,7 +53,7 @@ class Library:
             print(transaction)
 
 
-# Example usage of book:
+# Example usage of book agoo:
 library_system = Library() 
 
 library_system.add_book("Introduction to Python", "John Doe", 5) 
