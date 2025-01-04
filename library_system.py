86eb8eda52da891f114adf0558647f668faeaf1e
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2025-01-04 08:49:46 +0800

    Update library_system.py

--- library_system.py
+++ library_system.py
@@ -71,6 +71,6 @@ library_system.return_book(1, 2)
 library_system.display_books()
 library_system.display_patrons()
 library_system.display_transactions()
-# Agoo & MSFT Inc. 2024 v1.3Beta
-# 12/16/2024
+# Agoo & MSFT Inc. 2024 v1.5 Beta
+# 1/16/2024
  
